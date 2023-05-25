329485f3b07f1fbe90740b458e1ffc80ac159a87
Bossbazooca <126900603+Bossbazooca@users.noreply.github.com>
2023-05-24 22:31:13 -0400

    Add files via upload

---
+++ password.js
@@ -0,0 +1,17 @@
+var password = Math.floor(Math.random() * 999999999) + 10000000;
+document.getElementById("passwordID").innerHTML = password;
+
+function buttonClicked() {
+    "use strict";
+    
+    var input = document.getElementById("input").value;
+    
+    if (input == password) {
+            window.alert("Correctamudo");
+        }
+    else {
+            window.alert("wrong");
+        }
+        
+
+}
